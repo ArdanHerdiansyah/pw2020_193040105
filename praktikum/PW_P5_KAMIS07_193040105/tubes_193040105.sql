bbae6fa8a409f0e5480cbb18cd2194f6ccbf5f09
ArdanHerdiansyah <58681058+ArdanHerdiansyah@users.noreply.github.com>
2020-04-14 17:57:53 +0700

    nama database diganti

--- praktikum/PW_P5_KAMIS07_193040105/tubes_193040105.sql
+++ praktikum/PW_P5_KAMIS07_193040105/tubes_193040105.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2020 at 04:37 AM
+-- Generation Time: Apr 14, 2020 at 12:50 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.3.14
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pw_193040105`
+-- Database: `tubes_193040105`
 --
 
 -- --------------------------------------------------------
